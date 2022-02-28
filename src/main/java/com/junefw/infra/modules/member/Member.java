9acd0e6acaefc63337a0ca1bf0086934a9d70111
HDHun <pooekdma@naver.com>
2022-02-28 17:29:12 +0900

    db

---
+++ src/main/java/com/junefw/infra/modules/member/Member.java
@@ -0,0 +1,42 @@
+package com.junefw.infra.modules.member;
+
+public class Member {
+	
+	private String spitSeq;
+	private String spitItem;
+	private String spitItemCate;
+	private Integer spitDelNy;
+	private Integer spiiSeq;
+public Integer getSpiiSeq() {
+		return spiiSeq;
+	}
+	public void setSpiiSeq(Integer spiiSeq) {
+		this.spiiSeq = spiiSeq;
+	}
+public Integer getSpitDelNy() {
+		return spitDelNy;
+	}
+	public void setSpitDelNy(Integer spitDelNy) {
+		this.spitDelNy = spitDelNy;
+	}
+	//------------
+	public String getSpitSeq() {
+		return spitSeq;
+	}
+	public void setSpitSeq(String spitSeq) {
+		this.spitSeq = spitSeq;
+	}
+	public String getSpitItem() {
+		return spitItem;
+	}
+	public void setSpitItem(String spitItem) {
+		this.spitItem = spitItem;
+	}
+	public String getSpitItemCate() {
+		return spitItemCate;
+	}
+	public void setSpitItemCate(String spitItemCate) {
+		this.spitItemCate = spitItemCate;
+	}
+	
+}
