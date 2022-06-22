2d4524150d4f3e2ec82790b967c059062e4015db
HDHun <pooekdma@naver.com>
2022-06-22 13:14:11 +0900

    excel 다운

--- src/main/java/com/junefw/infra/modules/member/MemberController.java
+++ src/main/java/com/junefw/infra/modules/member/MemberController.java
@@ -13,8 +13,14 @@ import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -113,6 +119,62 @@ public class MemberController {
 		
 		  return "redirect:/member/memberList";
 	  }
+	 @RequestMapping("/member/excelDownload")
+	    public void excelDownload(MemberVo vo, HttpServletResponse httpServletResponse) throws Exception {
+			
+			vo.setShOptionDate(vo.getShOptionDate() == null ? 1 : vo.getShOptionDate());
+			vo.setShDateStart(vo.getShDateStart() == null
+					? UtilDateTime.calculateDayString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL)
+					: UtilDateTime.add00TimeString(vo.getShDateStart()));
+			vo.setShDateEnd(vo.getShDateEnd() == null ? UtilDateTime.nowString()
+					: UtilDateTime.addNowTimeString(vo.getShDateEnd()));
+
+			vo.setParamsPaging(service.selectOneCount(vo));
+
+			if (vo.getTotalRows() > 0) {
+				List<Member> list = service.selectList(vo);
+//				List<?> list = service.selectList(vo);
+
+			
+			
+		//        Workbook wb = new HSSFWorkbook();
+		        Workbook wb = new XSSFWorkbook();
+		        Sheet sheet = wb.createSheet("첫번째 시트");
+		        Row row = null;
+		        Cell cell = null;
+		        int rowNum = 0;
+		
+		        // Header
+		        row = sheet.createRow(rowNum++);
+		        cell = row.createCell(0);
+		        cell.setCellValue("번호");
+		        cell = row.createCell(1);
+		        cell.setCellValue("이름");
+		        cell = row.createCell(2);
+		        cell.setCellValue("제목");
+		
+		        // Body
+		        
+		        for (int i=0; i<list.size(); i++) {
+		            row = sheet.createRow(rowNum++);
+		            cell = row.createCell(0);
+		            cell.setCellValue(String.valueOf(list.get(i).getIfmmSeq()));
+		            cell = row.createCell(1);
+		            cell.setCellValue(list.get(i).getIfmmName());
+		            cell = row.createCell(2);
+		            cell.setCellValue(i+"_title");
+		        }
+		
+		        // 컨텐츠 타입과 파일명 지정
+		        httpServletResponse.setContentType("ms-vnd/excel");
+		//        response.setHeader("Content-Disposition", "attachment;filename=example.xls");
+		        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xlsx");
+		
+		        // Excel File Output
+		        wb.write(httpServletResponse.getOutputStream());
+		        wb.close();
+			}
+	    }
 	  
 	  @RequestMapping(value = "/member/memberView")
 	  public String memberView(@ModelAttribute("vo") MemberVo vo, Model model) throws Exception {
